b3326762ac6a7cc2cb885c744fd3a89497dc60f0
coderInPractice <coderInPractice@noreply.github.com>
2019-12-06 05:08:14 +0530

    Xylophone
    A website project on Xylophone which play sound on click.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var audio1 = new Audio ();
+var audio2 = new Audio ();
+var audio3 = new Audio ();
+var audio4 = new Audio ();
+var audio5 = new Audio ();
+var audio6 = new Audio ();
+var audio7 = new Audio ();
+
+
+audio1.src = 'note1.mp3';
+audio2.src = 'note2.mp3';
+audio3.src = 'note3.mp3';
+audio4.src = 'note4.mp3';
+audio5.src = 'note5.mp3';
+audio6.src = 'note6.mp3';
+audio7.src = 'note7.mp3';
+
+function playSound(soundfile) {
+    audio1.play();
+    audio2.play();
+    audio3.play();
+    audio4.play();
+    audio5.play();
+    audio6.play();
+    audio7.play();
+  }
